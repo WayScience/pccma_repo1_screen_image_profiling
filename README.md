1fa8c9768e36a1e591869fb6c8ed75f9cc636b90
jenna-tomkinson <jenna.tomkinson@ucdenver.edu>
2026-03-30 14:47:15 -0600

    add workflows to run SK-N-AS repo1 screen

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# pccma_repo1_screen_image_profiling
+# Pediatric Cancer Cell Morphology Atlas REPO1 Drug Screens
+
+This repository holds all of the pipelines and workflows for image analysis and image-based profiling for REPO1 drug screen for specific pediatric cancer cell lines.
---
+++ SK-N-AS_repo1_screen/1.illumination_correction/cp_illum_correction_hpc_child.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem=10G
+#SBATCH --partition=amilan
+#SBATCH --qos=long
+#SBATCH --account=amc-general
+#SBATCH --time=03:00:00
+#SBATCH --output=run_CP_child-%j.out
+
+# 1 task at 10GB RAM for the core (adjust as needed)
+
+# activate cellprofiler environment
+module load miniforge
+conda init bash
+conda activate pccma_repo1_cp_env
+
+# input csv  passed as first argument
+csv=$1
+
+# convert notebooks to python scripts
+jupyter nbconvert --to=script --FilesWriter.build_directory=nbconverted/ *.ipynb
+
+# run your python illumination correction script with the input csv
+python nbconverted/1.cp_illum_correction_hpc.py --input_csv "$csv"
+
+# deactivate conda environment
+conda deactivate
+
+echo "CellProfiler illumination correction done for directory: $csv"
